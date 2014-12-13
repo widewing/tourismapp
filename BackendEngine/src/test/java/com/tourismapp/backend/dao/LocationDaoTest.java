7293fa3ef8b7ba01e7980cefd5674b82f3dbb2a1
Adun Wang <arbiterx@gmail.com>
2014-12-13 13:41:29 +0800

    init spring data jpa

---
+++ BackendEngine/src/test/java/com/tourismapp/backend/dao/LocationDaoTest.java
@@ -0,0 +1,39 @@
+package com.tourismapp.backend.dao;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LocationDaoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindOne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+}
