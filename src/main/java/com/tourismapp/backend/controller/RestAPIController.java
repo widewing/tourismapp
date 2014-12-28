728e8d066cc979ca185a5fe7d02019ca6a1f9ecb
gengwang <gengwang@gengwang-WS.cisco.com>
2014-12-29 01:42:07 +0800

    change to spring 3

--- src/main/java/com/tourismapp/backend/controller/RestAPIController.java
+++ src/main/java/com/tourismapp/backend/controller/RestAPIController.java
@@ -5,17 +5,17 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.tourismapp.backend.entity.Activity;
 import com.tourismapp.backend.service.ActivityService;
 
-@RestController
-@RequestMapping("api/")
+@Controller
+@RequestMapping("/api/")
 public class RestAPIController {
 	@Autowired
 	private ActivityService activityService;
