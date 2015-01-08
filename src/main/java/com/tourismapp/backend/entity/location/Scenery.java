512d0afd3960d2167c6d394ac31812c222dd826b
gengwang <gengwang@gengwang-WS.cisco.com>
2015-01-08 22:53:03 +0800

    add many to one of city

--- src/main/java/com/tourismapp/backend/entity/location/Scenery.java
+++ src/main/java/com/tourismapp/backend/entity/location/Scenery.java
@@ -2,16 +2,25 @@ package com.tourismapp.backend.entity.location;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "scenery")
 public class Scenery extends BaseLocationEntity {
+	private City city;
 	private float closeTime;
 	private float openTime;
 	private float price;
 	private int rank;
 	private int visitMinutes;
+
+	@ManyToOne
+	@JoinColumn(name = "city_id", nullable = false)
+	public City getCity() {
+		return city;
+	}
 
 	@Column(name = "close_time")
 	public float getCloseTime() {
@@ -35,6 +44,10 @@ public class Scenery extends BaseLocationEntity {
 
 	public int getVisitMinutes() {
 		return visitMinutes;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
 	}
 
 	public void setCloseTime(float closeTime) {
