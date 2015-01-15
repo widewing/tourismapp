215769407a6923f60857a77fe8b16a3a8ae21eb3
Gavin Ni <gisngy@gmail.com>
2015-01-15 22:31:20 +0800

    Add some comments in route engine
    Signed-off-by: Gavin Ni <gisngy@gmail.com>

--- src/main/java/com/tourismapp/backend/engine/Session.java
+++ src/main/java/com/tourismapp/backend/engine/Session.java
@@ -79,6 +79,7 @@ public class Session {
 	TransportationDataService transportation = new TransportationDataService();
 	HotelDto lastNightHotel;
 
+	//安排一个城市中的行程
 	private List<RouteSection> arrangeForDistrict(District district, Collection<SceneryDto> sceneries,
 			Location startLocation, Date startTime) {
 		Collection<SceneryDto> remained = new HashSet<SceneryDto>(sceneries);
@@ -114,6 +115,7 @@ public class Session {
 		return routes;
 	}
 
+	//根据当前位置和时间安排住宿，如果上一晚位置较近则安排上一晚酒店
 	private List<RouteSection> arrangeHotel(Date currentTime, Location currentLoc) {
 		if (lastNightHotel != null && lastNightHotel.getCoord().distance(currentLoc.getCoord()) < 20000)
 			return arrangeVisit(currentLoc, currentTime, lastNightHotel, nextMorning(currentTime), lastNightHotel
@@ -132,12 +134,14 @@ public class Session {
 				bestHotel.getPrices().get(Hotel.RoomType.SINGLE.name()));
 	}
 
+	//安排市内交通
 	private List<TransportSection> arrangeInnerCityTransport(Location leaveLoc, Location arriveLoc, Date leaveTime) {
 		Collection<Transport> availableTransports = transportation.getAvailableTransports(leaveTime, leaveLoc,
 				arriveLoc, new Transport.Method[] { Transport.Method.Bus, Transport.Method.Taxi });
 		return selectTransport(leaveLoc, arriveLoc, leaveTime, availableTransports);
 	}
 
+	//安排城际交通
 	private List<TransportSection> arrangeInterCityTransport(Location leaveLoc, District arriveLoc, Date leaveTime) {
 		Collection<Transport> availableTransports = transportation.getAvailableTransports(leaveTime, leaveLoc,
 				arriveLoc,
@@ -145,6 +149,7 @@ public class Session {
 		return selectTransport(leaveLoc, arriveLoc, leaveTime, availableTransports);
 	}
 
+	//安排吃饭地
 	private List<RouteSection> arrangeRestaurant(Location currentLoc, Date currentTime, Location destination) {
 		List<Coord> availCoords = new ArrayList<Coord>();
 		if (currentLoc instanceof SceneryDto && ((SceneryDto) currentLoc).getEntrances() != null)
@@ -170,6 +175,7 @@ public class Session {
 		return arrangeVisit(currentLoc, currentTime, bestRestaurant, 40, bestRestaurant.getEstimatedCost());
 	}
 
+	//安排游览景点（给定结束时间）
 	private List<RouteSection> arrangeVisit(Location currentLoc, Date currentTime, Location visitLoc, Date visitUntil,
 			float destinationPrice) {
 		LinkedList<RouteSection> routes = new LinkedList<RouteSection>();
@@ -178,6 +184,7 @@ public class Session {
 		return routes;
 	}
 
+	//安排游览景点（给定游览时间）
 	private List<RouteSection> arrangeVisit(Location currentLoc, Date currentTime, Location visitLoc,
 			int durationMinutes, float destinationPrice) {
 		LinkedList<RouteSection> routes = new LinkedList<RouteSection>();
@@ -186,6 +193,7 @@ public class Session {
 		return routes;
 	}
 
+	//将景点按城市划分
 	private Map<District, Collection<SceneryDto>> calcDistricts() {
 		Map<District, Collection<SceneryDto>> districts = new HashMap<District, Collection<SceneryDto>>();
 		for (SceneryDto s : selectedSceneries) {
@@ -197,6 +205,7 @@ public class Session {
 		return districts;
 	}
 
+	//计算城市旅行顺序
 	private List<District> calcDistrictVisitOrder(Map<District, Collection<SceneryDto>> groups) {
 		// Map<District,Integer> visitDays =
 		// calcEstimatedDaysForDistricts(groups);
@@ -204,6 +213,7 @@ public class Session {
 		return new ArrayList<District>(groups.keySet());
 	}
 
+	//计算每个城市的预计停留时间
 	private Map<District, Integer> calcEstimatedDaysForDistricts(Map<District, Collection<Scenery>> groups) {
 		Map<District, Integer> result = new HashMap<District, Integer>();
 		int estimatedTotalHours = 0;
@@ -231,6 +241,7 @@ public class Session {
 		return result;
 	}
 
+	//计算景点的预计游览时间
 	private int calcEstimatedHours(Collection<Scenery> sceneries) {
 		int minutes = 0;
 		for (Scenery s : sceneries)
@@ -238,21 +249,25 @@ public class Session {
 		return (int) ((minutes * visitTimeCoefficient + 40 * sceneries.size()) / 60);
 	}
 
+	//计算tag的相合度
 	private float calcTagsFitness(Set<String> tags1, Set<String> tags2) {
 		// TODO
 		return 0;
 	}
 
+	//获取所有可能的城际交通路线
 	private Collection<ScheduledTransport> getAvailableTransports(Collection<District> districts, Date startTime, Date endTime) {
 		// TODO
 		return null;
 	}
 
+	//获取每个城市的预计酒店价格
 	private Map<District, Map<Date, Integer>> getEstimatedHotelPrices() {
 		// TODO
 		return null;
 	}
 
+	//从当前点寻找合适的下一个游览景点，如不存在返回null
 	private SceneryDto getNextScenery(float hourInDay, Location currentLoc, Collection<SceneryDto> sceneries) {
 		SceneryDto nearestScenery = null;
 		double nearestDistance = 0;
@@ -279,6 +294,7 @@ public class Session {
 		return nearestScenery;
 	}
 
+	//主入口
 	public List<RouteSection> makeTravelRoute() {
 		Map<District, Collection<SceneryDto>> sceneryDistricts = calcDistricts();
 		List<District> districts = calcDistrictVisitOrder(sceneryDistricts);
@@ -304,6 +320,7 @@ public class Session {
 		return routes;
 	}
 
+	//第二天出发时间
 	private Date nextMorning(Date currentTime) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(currentTime);
@@ -315,6 +332,7 @@ public class Session {
 		return cal.getTime();
 	}
 
+	//选择最合适的交通方式
 	private List<TransportSection> selectTransport(Location leaveLoc, Location arriveLoc, Date leaveTime,
 			Collection<Transport> availableTransports) {
 		Transport bestTransport = null;
